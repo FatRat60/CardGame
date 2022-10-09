fcb995d23faa45fcf404d4b8d61f83eba82fd292
FatRat60 <kylehultgren99@yahoo.com>
2022-10-09 16:04:36 -0700

    Added cute little images to main menu

--- CardGame/CardGameApp.swift
+++ CardGame/CardGameApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CardGameApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainMenuView()
         }
     }
 }
--- CardGame/MainMenuView.swift
+++ CardGame/MainMenuView.swift
@@ -18,7 +18,20 @@ struct MainMenuView: View {
                 Spacer()
                 Text("FatRat60's Card Game")
                     .font(.system(size: 28, weight: .bold))
-                Spacer()
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5){
+                    Image("kokomi")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image("shinoa")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image("keqing")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                     Text("Singleplayer")
                         .padding()
